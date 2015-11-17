e79bc0c8bcd0f331f8d150a6d63fbe91de796406
Miguel Muscat <miguelmuscat93@gmail.com>
2015-11-17 16:12:34 +0100

    Added main file

---
+++ edd-add-to-cart-popup.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name: Easy Digital Downloads - Add to Cart Popup
+ * Description: An Easy Digital Downloads extension that adds a customizable popup when your customers click the "Add toCart" button, with options to navigate to the Shopping Cart, to Checkout or to keep on shopping. <strong>Requires PHP 5.3 or later.</strong>
+ * Version: 0.0
+ * Author: Jean Galea
+ * Contributors: Miguel Muscat
+ */
+
