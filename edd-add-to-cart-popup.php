d0c65ef15547ff101c7a2b085febc3f802726ef2
Miguel Muscat <miguelmuscat93@gmail.com>
2015-11-17 17:08:47 +0100

    Updated description in plugin header (EDDACP-1)

--- edd-add-to-cart-popup.php
+++ edd-add-to-cart-popup.php
@@ -3,7 +3,7 @@
 /**
  * @wordpress-plugin
  * Plugin Name: Easy Digital Downloads - Add to Cart Popup
- * Description: An Easy Digital Downloads extension that adds a customizable popup when your customers click the "Add toCart" button, with options to navigate to the Shopping Cart, to Checkout or to keep on shopping. <strong>Requires PHP 5.3 or later.</strong>
+ * Description: An Easy Digital Downloads extension for showing a lightbox popup when customers click the "Add to Cart" button. <strong>Requires PHP 5.3 or later.</strong>
  * Version: 0.0
  * Author: Jean Galea
  * Contributors: Miguel Muscat
