8aa530cac51b9e2d98936988427769f81f70f95c
Miguel Muscat <miguelmuscat93@gmail.com>
2015-11-20 12:53:06 +0100

    Extended HookLoader (EDDACP-4)
    - Now allows hooks to be added instantly or queued
    - Old register function now only registeres queued hooks

--- includes/Aventura/Edd/AddToCartPopup/HookLoader.php
+++ includes/Aventura/Edd/AddToCartPopup/HookLoader.php
@@ -9,9 +9,12 @@ namespace Aventura\Edd\AddToCartPopup;
  * the plugin, and register them with the WordPress API. Call the
  * run function to execute the list of actions and filters.
  *
- * @version 1.0.0
+ * @version 1.1.0
  */
 class HookLoader {
+
+	const TYPE_ACTION = 'action';
+	const TYPE_FILTER = 'filter';
 
 	/**
 	 * The array of actions registered with WordPress.
@@ -52,7 +55,21 @@ class HookLoader {
 	 * @var      int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
 	 */
 	public function addAction( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
-		$this->actions = $this->add( $this->actions, $hook, $component, $callback, $priority, $accepted_args );
+		return $this->add( self::TYPE_ACTION, false, $hook, $component, $callback, $priority, $accepted_args );
+	}
+
+	/**
+	 * Queues a new action to the collection to be registered later with WordPress.
+	 *
+	 * @since    1.1.0
+	 * @var      string               $hook             The name of the WordPress action that is being registered.
+	 * @var      object               $component        A reference to the instance of the object on which the action is defined.
+	 * @var      string               $callback         The name of the function definition on the $component.
+	 * @var      int      Optional    $priority         The priority at which the function should be fired.
+	 * @var      int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
+	 */
+	public function queueAction( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
+		return $this->add( self::TYPE_ACTION, true, $hook, $component, $callback, $priority, $accepted_args );
 	}
 
 	/**
@@ -66,7 +83,21 @@ class HookLoader {
 	 * @var      int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
 	 */
 	public function addFilter( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
-		$this->filters = $this->add( $this->filters, $hook, $component, $callback, $priority, $accepted_args );
+		return $this->add( self::TYPE_FILTER, false, $hook, $component, $callback, $priority, $accepted_args );
+	}
+
+	/**
+	 * Queues a new filter to the collection to be registered later with WordPress.
+	 *
+	 * @since    1.1.0
+	 * @var      string               $hook             The name of the WordPress filter that is being registered.
+	 * @var      object               $component        A reference to the instance of the object on which the filter is defined.
+	 * @var      string               $callback         The name of the function definition on the $component.
+	 * @var      int      Optional    $priority         The priority at which the function should be fired.
+	 * @var      int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
+	 */
+	public function queueFilter( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
+		return $this->add( self::TYPE_FILTER, true, $hook, $component, $callback, $priority, $accepted_args );
 	}
 
 	/**
@@ -74,8 +105,9 @@ class HookLoader {
 	 * collection.
 	 *
 	 * @since    1.0.0
-	 * @access   private
-	 * @var      array                $hooks            The collection of hooks that is being registered (that is, actions or filters).
+	 * @access   protected
+	 * @var      string               $type             The hook type: 'filter' or 'action'
+	 * @var      boolean              $queue            If true, the hook is queued to be registered later. If false, it is hooked immediately.
 	 * @var      string               $hook             The name of the WordPress filter that is being registered.
 	 * @var      object               $component        A reference to the instance of the object on which the filter is defined.
 	 * @var      string               $callback         The name of the function definition on the $component.
@@ -83,15 +115,25 @@ class HookLoader {
 	 * @var      int      Optional    $accepted_args    The number of arguments that should be passed to the $callback.
 	 * @return   type                                   The collection of actions and filters registered with WordPress.
 	 */
-	private function add( $hooks, $hook, $component, $callback, $priority, $accepted_args ) {
-		$hooks[] = array(
-			'hook'          => $hook,
-			'component'     => $component,
-			'callback'      => $callback,
-			'priority'      => $priority,
-			'accepted_args' => $accepted_args
-		);
-		return $hooks;
+	protected function add( $type, $queue, $hook, $component, $callback, $priority, $accepted_args ) {
+		if ($queue) {
+			// Add to appropriate array property
+			$property = sprintf('%ss', $type);
+			$this->{$property}[] = array(
+				'hook'          => $hook,
+				'component'     => $component,
+				'callback'      => $callback,
+				'priority'      => $priority,
+				'accepted_args' => $accepted_args
+			);
+		} else {
+			// Call function for hooking
+			$fn = sprintf('add_%s', $type);
+			$args = array($hook, array($component, $callback), $priority, $accepted_args);
+			call_user_func_array($fn, $args);
+		}
+		// Return instance for chaining
+		return $this;
 	}
 
 	/**
@@ -99,12 +141,12 @@ class HookLoader {
 	 *
 	 * @since    1.0.0
 	 */
-	public function registerHooks() {
+	public function registerQueue() {
 		foreach ( $this->filters as $hook ) {
-			add_filter( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+			$this->add( self::TYPE_FILTER, false, $hook['hook'], $hook['component'], $hook['callback'], $hook['priority'], $hook['accepted_args'] );
 		}
 		foreach ( $this->actions as $hook ) {
-			add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+			$this->add( self::TYPE_ACTION, false, $hook['hook'], $hook['component'], $hook['callback'], $hook['priority'], $hook['accepted_args'] );
 		}
 	}
 
