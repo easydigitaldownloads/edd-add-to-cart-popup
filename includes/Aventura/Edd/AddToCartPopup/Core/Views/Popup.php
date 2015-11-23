7f2dd8ca807a33c9082a84711fcf7e9c87196bf6
Miguel Muscat <miguelmuscat93@gmail.com>
2015-11-23 14:33:47 +0100

    Adde Popup view file (EDDACP-8)

---
+++ includes/Aventura/Edd/AddToCartPopup/Core/Views/Popup.php
@@ -0,0 +1,11 @@
+<div id="edd-acp-popup">
+	<p>
+		<strong class="item-name"></strong> has been added to your cart!
+	</p>
+	<p>
+		<a href="#" class="edd-acp-goto-checkout">
+			<button class="button">Proceed to Checkout</button>
+		</a>
+		<button class="button edd-acp-close-popup">Continue Shopping</button>
+	</p>
+</div>
