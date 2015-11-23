320627299458131440a1f02d9ffaf3824fc098b3
Miguel Muscat <miguelmuscat93@gmail.com>
2015-11-23 19:04:45 +0100

    Updated Popup View (EDDACP-3)

--- includes/Aventura/Edd/AddToCartPopup/Core/Views/Popup.php
+++ includes/Aventura/Edd/AddToCartPopup/Core/Views/Popup.php
@@ -1,3 +1,13 @@
+<?php
+	$settings = $this->getPlugin()->getSettings();
+?>
+<style type="text/css">
+div#edd-acp-popup {
+	color: <?php echo $settings->getSubValue('textcolor', 'black'); ?>;
+	background: <?php echo $settings->getSubValue('bgcolor', 'white'); ?>;
+}
+</style>
+
 <div id="edd-acp-popup">
 	<p>
 		<strong class="item-name"></strong> has been added to your cart!
