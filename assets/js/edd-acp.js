485b9bf34ad060abe1c5c1d9baab95fa91de6d38
Miguel Muscat <miguelmuscat93@gmail.com>
2015-11-23 14:49:39 +0100

    Updated popup JS (EDDACP-8)
    - JS structured in OO way
    - Uses HTML from view

--- assets/js/edd-acp.js
+++ assets/js/edd-acp.js
@@ -1,11 +1,43 @@
-;(function($) {
+var EddAcp = (function EddAcpClass() {
 
-	var eddAddToCartClick = function(event) {
-		console.log('Clicked the "Add to Cart" button!');
+	EddAcp.prototype = Object.create(Object.prototype);
+
+	var $ = jQuery;
+
+	function EddAcp() {
+		this.initElems();
+		this.initEvents();
+		this.testing = false;
+	}
+
+	EddAcp.prototype.initElems = function() {
+		this.popup = $('#edd-acp-popup');
+		this.eddPurchaseWrapper = $('.edd_purchase_submit_wrapper');
+		this.eddPurchaseButton = this.eddPurchaseWrapper.find('a.edd-add-to-cart');
+		this.itemName = $('h1.entry-title > span').text();
+		this.popup.find('strong.item-name').text(this.itemName);
+		this.popup.find('a.edd-acp-goto-checkout').attr('href', edd_scripts.checkout_page);
 	};
 
-	$(document).ready(function() {
-		$('.edd_purchase_submit_wrapper > a.edd-add-to-cart').click(eddAddToCartClick);
-	});
+	EddAcp.prototype.initEvents = function() {
+		this.eddPurchaseButton.click(this.onPurchaseClick.bind(this));
+	};
 
-})(jQuery);
+	EddAcp.prototype.onPurchaseClick = function(event) {
+		this.popup.bPopup({
+			positionStyle: 'fixed',
+			speed: 100,
+			closeClass: 'edd-acp-close-popup'
+		});
+		if (this.testing) {
+			event.stopPropagation();
+			event.preventDefault();
+		}
+	};
+
+	return EddAcp;
+}());
+
+jQuery(document).ready( function() {
+	window.eddAcp = new EddAcp();
+});
