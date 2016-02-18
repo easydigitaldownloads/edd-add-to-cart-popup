1cdf151693774f0531cd3ce2d7037d78956b307f
Miguel Muscat <miguelmuscat93@gmail.com>
2016-02-18 13:52:47 +0100

    Fixed output bug in popup (EDDACP-18)
    - Whitespace was being displayed as a hyperlink
    
    EDDACP-18 #time 5m

--- includes/Aventura/Edd/AddToCartPopup/Core/Views/Popup.php
+++ includes/Aventura/Edd/AddToCartPopup/Core/Views/Popup.php
@@ -12,11 +12,7 @@
 		<?php printf($settings->getSubValue('maintext'), '<strong class="item-name"></strong>'); ?>
 	</p>
 	<p>
-		<a href="#" class="edd-acp-goto-checkout">
-			<button class="button">
-				<?php echo $settings->getSubValue('checkoutBtnText'); ?>
-			</button>
-		</a>
+		<a href="#" class="edd-acp-goto-checkout"><button class="button"><?php echo $settings->getSubValue('checkoutBtnText'); ?></button></a>
 		<button class="button edd-acp-close-popup">
 			<?php echo $settings->getSubValue('continueBtnText'); ?>
 		</button>
