453d5953cfa24e848df0e29fa207218747fee38d
Miguel Muscat <miguelmuscat93@gmail.com>
2016-03-21 13:35:55 +0100

    Item name shows selected price option (EDDACP-20)
    EDDACP-20 #time 1h

--- assets/js/edd-acp.js
+++ assets/js/edd-acp.js
@@ -19,8 +19,8 @@ var EddAcp = (function EddAcpClass() {
 		this.popup = this.element.find('.edd-acp-popup');
 		// Get the item name
 		this.itemName = this.popup.find('.edd-acp-item-name').val();
-		// Set the item name
-		this.popup.find('strong.item-name').text(this.itemName);
+		// Get the variable price option element, if available
+		this.priceOptions = this.element.find('div.edd_price_options');
 		// Set the url of the "continue to checkout button" to the checkout page
 		this.popup.find('a.edd-acp-goto-checkout').attr('href', edd_scripts.checkout_page);
 
@@ -34,6 +34,36 @@ var EddAcp = (function EddAcpClass() {
 	};
 
 	EddAcp.prototype.onPurchaseClick = function(event) {
+		// Item name to show on popup
+		var name = this.itemName;
+		// Get selected price options
+		var priceOptions = this.getSelectedPriceOption();
+		// If no selection, or variable pricing disabled
+		if (priceOptions !== null) {
+			// Use hyphen between item name and selected options
+			name += ' - ';
+			// Get the default quantity field
+			var eddQtyField = this.element.find('.edd-item-quantity');
+			// Put selections in array, to join by comma later
+			var optionStrings = [];
+			for (var i in priceOptions) {
+				// If the quantity is 1 and only one option is selected, do not show quantity
+				var qtyStr = '';
+				if (priceOptions[i].qty > 1 || priceOptions.length > 1) {
+					qtyStr = ' x' + priceOptions[i].qty;
+				} else if (eddQtyField.length > 0 && parseInt(eddQtyField.val()) > 1) {
+					qtyStr = ' x' + eddQtyField.val();
+				}
+				// Generate string for selected price option and add to array
+				var optionString = ' ' + priceOptions[i].name + qtyStr;
+				optionStrings.push(optionString);
+			}
+			// Join options by comma and add to item name
+			name += optionStrings.join(', ');
+		}
+		// Set the item name on popup element
+		this.popup.find('strong.item-name').text(name);
+		// Show the popup
 		this.popup.bPopup({
 			positionStyle: 'fixed',
 			speed: 100,
@@ -43,6 +73,34 @@ var EddAcp = (function EddAcpClass() {
 			event.stopPropagation();
 			event.preventDefault();
 		}
+	};
+
+	EddAcp.prototype.getSelectedPriceOption = function() {
+		if (this.priceOptions.length === 0) {
+			return null;
+		}
+		var selected = [];
+		// For each price option
+		this.priceOptions.find('> ul > li').each(function(i, l) {
+			// Get the label and quantity wrapper
+			var label = $(l).find('> label');
+			var qtyWrapper = $(l).find('> div.edd_download_quantity_wrapper');
+			// If the option is selected
+			if (label.find('input').is('[type="checkbox"]:checked, [type="radio"]:checked')) {
+				// Get the option name and entered quantity
+				var name = label.find('.edd_price_option_name').text();
+				var qty = qtyWrapper.length === 0
+					? 1
+					: parseInt(qtyWrapper.find('input.edd-item-quantity').val());
+				// Add selection
+				selected.push({
+					name: name,
+					qty: qty
+				});
+			}
+		});
+		console.log(selected);
+		return selected;
 	};
 
 	return EddAcp;
