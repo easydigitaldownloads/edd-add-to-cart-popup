1fcd9001e80949b20d5055cc007c839a99bf1d92
Miguel Muscat <miguelmuscat93@gmail.com>
2016-07-29 18:51:57 +0200

    Changed plugin author to RebelCode (EDDACP-33)
    EDDACP-33 #time 1m

--- edd-add-to-cart-popup.php
+++ edd-add-to-cart-popup.php
@@ -5,7 +5,7 @@
  * Plugin Name: Easy Digital Downloads - Add to Cart Popup
  * Description: An Easy Digital Downloads extension for showing a lightbox popup when customers click the "Add to Cart" button. <strong>Requires PHP 5.3 or later.</strong>
  * Version: 1.0.0
- * Author: Jean Galea
+ * Author: RebelCode
  * Contributors: Miguel Muscat
  */
 
