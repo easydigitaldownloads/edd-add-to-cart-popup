b00edaac4a63328e12d067777396d7632b54e81c
Miguel Muscat <miguelmuscat93@gmail.com>
2016-08-30 13:15:27 +0200

    Settings JS uses spectrum (EDDACP-48)
    EDDACP-48 #time 15m

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -3,39 +3,18 @@
     $(document).ready(function () {
 
         // Iterate all colorpicker containers
-        $('.edd-acp-colorpicker').each(function () {
-            // Prepare element pointers
+        $('.edd-acp-colorpicker').each(function() {
             var self = $(this);
-            var value = self.find('input.edd-acp-colorpicker-value');
-            var preview = self.find('div.edd-acp-colorpicker-preview');
-
-            // Colorpicker preview and value updater function
-            var updateColorpicker = function (hsb, hex, rgb) {
-                value.val('#' + hex);
-                preview.css('backgroundColor', '#' + hex);
-            };
-            // Update element first time
-            updateColorpicker(null, value.val().substr(1), null);
-
-            // Set up color picker
-            self.ColorPicker({
-                // Set color before showing colorpicker
-                onBeforeShow: function () {
-                    $(this).ColorPickerSetColor(value.val());
-                },
-                // Fade in effect
-                onShow: function (colorpicker) {
-                    $(colorpicker).fadeIn(200);
-                    return false;
-                },
-                // Fade out effect
-                onHide: function (colorpicker) {
-                    $(colorpicker).fadeOut(200);
-                    return false;
-                },
-                // Update color picker preview and value on change and submit
-                onSubmit: updateColorpicker,
-                onChange: updateColorpicker
+            var field = self.find('input.edd-acp-colorpicker-value');
+            field.spectrum({
+                color: field.val(),
+                allowEmpty: true,
+                showAlpha: true,
+                showPalette: true,
+                palette: [],
+                localStorageKey: 'spectrum.homepage',
+                showInitial: true,
+                preferredFormat: 'rgb',
             });
         });
 
